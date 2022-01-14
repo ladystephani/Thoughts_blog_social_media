15e35fd29d03ee179f01e187125de5f9a9ccbc14
Lady Gaga <jessiezheqi.li@gmail.com>
2022-01-13 22:05:02 -0800

    Add thoughts & friends to user model

--- models/User.js
+++ models/User.js
@@ -1,19 +1,45 @@
 const { Schema, model } = require("mongoose");
+const Thought = require("./Thought");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //validation
+      match: [/.+@.+\..+/, "Must be an email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    //validation
-    match: [/.+@.+\..+/, "Must be an email address!"],
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//for number of friends, count with a virtual property
+userSchema.virtual("countFriends").get(function () {
+  return this.friends.length;
 });
 
 const User = model("User", userSchema);
